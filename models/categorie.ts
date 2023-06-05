51067273fdc022c9f5786f23ba2fb5609e03ceba
bflorestal <yayou7710@hotmail.fr>
2023-06-05 15:47:38 +0200

    Ajout des models de l'énoncé

---
+++ models/categorie.ts
@@ -0,0 +1,13 @@
+class Categorie {
+  id: string;
+  genre: string;
+  couleur: string;
+
+  constructor(id: string, genre: string, couleur: string) {
+    this.id = id;
+    this.genre = genre;
+    this.couleur = couleur;
+  }
+}
+
+export default Categorie;
---
+++ models/livre.ts
@@ -0,0 +1,29 @@
+class Livre {
+  id: string;
+  categorieId: string[];
+  titre: string;
+  description: string;
+  tomes: number;
+  imageUrl: string;
+  enCours: boolean;
+
+  constructor(
+    id: string,
+    categorieId: string[],
+    titre: string,
+    description: string,
+    tomes: number,
+    imageUrl: string,
+    enCours: boolean
+  ) {
+    this.id = id;
+    this.categorieId = categorieId;
+    this.titre = titre;
+    this.description = description;
+    this.tomes = tomes;
+    this.imageUrl = imageUrl;
+    this.enCours = enCours;
+  }
+}
+
+export default Livre;
