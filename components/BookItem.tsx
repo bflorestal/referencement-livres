ec7e81619337e63b4f1aa207ad92d652dbbcf340
bflorestal <yayou7710@hotmail.fr>
2023-06-06 10:09:59 +0200

    Barre de recherche et affichage des livres

---
+++ components/BookItem.tsx
@@ -0,0 +1,101 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+
+import Livre from "../models/livre";
+import { CATEGORIES } from "../models/data";
+
+function getCategoryName(categoryId: string) {
+  return CATEGORIES.find((category) => category.id === categoryId)?.genre;
+}
+
+function getCategoryColor(categoryId: string) {
+  const category = CATEGORIES.find((category) => category.id === categoryId);
+  return category?.couleur;
+}
+
+export default function BookItem({ book }: { book: Livre }) {
+  return (
+    <View style={styles.bookItemContainer}>
+      <View style={styles.bookImageContainer}>
+        <Image
+          source={{ uri: book.imageUrl }}
+          alt={book.titre}
+          style={styles.bookImage}
+        />
+      </View>
+      <View style={styles.bookDescriptionContainer}>
+        <View>
+          <Text style={styles.bookTitle}>{book.titre}</Text>
+          <Text
+            style={[
+              styles.smallText,
+              { color: book.enCours ? "green" : "blue" },
+            ]}
+          >
+            {book.enCours ? "En cours" : "Terminé"}
+          </Text>
+          <Text style={styles.smallText}>Nombre de tomes : {book.tomes}</Text>
+        </View>
+        <View>
+          <Text style={styles.categoriesListTitle}>Catégories :</Text>
+          <View style={styles.categoriesListContainer}>
+            {book.categorieId.map((categoryId) => (
+              <Text
+                style={[
+                  styles.categoryPill,
+                  { backgroundColor: getCategoryColor(categoryId) || "#000" },
+                  styles.smallText,
+                ]}
+                key={categoryId}
+              >
+                {getCategoryName(categoryId)}
+              </Text>
+            ))}
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const rightMaxWidth = 100;
+
+const styles = StyleSheet.create({
+  bookItemContainer: {
+    backgroundColor: "#fff",
+    flexDirection: "row",
+  },
+  bookImageContainer: {
+    maxWidth: rightMaxWidth,
+    gap: 5,
+  },
+  bookImage: {
+    width: rightMaxWidth,
+    aspectRatio: 3 / 4,
+  },
+  bookTitle: {
+    fontWeight: "600",
+  },
+  bookDescriptionContainer: {
+    padding: 10,
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-between",
+  },
+  smallText: {
+    fontSize: 12,
+  },
+  categoriesListTitle: {
+    fontWeight: "500",
+  },
+  categoriesListContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    gap: 6,
+  },
+  categoryPill: {
+    borderRadius: 10,
+    paddingVertical: 3,
+    paddingHorizontal: 8,
+    color: "#fff",
+  },
+});
--- pages/Search.tsx
+++ pages/Search.tsx
@@ -1,10 +1,30 @@
 import { FlatList, StyleSheet, Text, TextInput, View } from "react-native";
+import { useState } from "react";
+
+import Livre from "../models/livre";
+import { LIVRES } from "../models/data";
+
+import BookItem from "../components/BookItem";
 
 export default function Search() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [books, setBooks] = useState<Livre[]>(LIVRES);
+
+  const handleChangeText = (text: string) => {
+    setSearchTerm(text);
+  };
+
+  const filteredBooks = books.filter((book) =>
+    book.titre.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <View style={styles.searchContainer}>
       <Text style={styles.title}>Rechercher un livre</Text>
       <TextInput
+        placeholder="Titre du livre"
+        value={searchTerm}
+        onChangeText={handleChangeText}
         style={{
           backgroundColor: "#fff",
           height: 40,
@@ -13,7 +33,12 @@ export default function Search() {
           borderRadius: 5,
           padding: 5,
         }}
-        placeholder="Titre du livre"
+      />
+      <FlatList
+        data={filteredBooks}
+        renderItem={({ item }) => <BookItem book={item} />}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={styles.bookItemList}
       />
     </View>
   );
@@ -30,5 +55,8 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 24,
     fontWeight: "700",
+  },
+  bookItemList: {
+    gap: 10,
   },
 });
